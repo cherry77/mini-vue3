724ebfe50c96c389e16761d6cd86e43f6dafc1bd
flora <15071285862@163.com>
2022-09-03 15:15:45 +0800

    add effect stop && onStop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,47 +1,84 @@
-class ReactiveEffect{
-  private _fn: any;
+import { extend } from "../shared"
 
-  // public scheduler? 这种写法需要注意下，之前不知道
-  constructor(fn, public scheduler?){
-    this._fn = fn
-    this.scheduler = scheduler
-  }
-  run(){
-    activeEffect = this
-    return this._fn() // 调用结果要返回下
-  }
-}
-const targetMap = new Map()
-export function track(target, key){
-  let depsMap = targetMap.get(target)
-  if(!depsMap){
-    depsMap = new Map()
-    targetMap.set(target, depsMap)
-  }
-  let deps = depsMap.get(key)
-  if(!deps){
-    deps = new Set()
-    depsMap.set(key, deps)
-  }
-  deps.add(activeEffect)
-}
+class ReactiveEffect {
+	private _fn: any
+	deps = []
+  active = true
+  onStop?: () => void // ts在类中定义方法
 
-export function trigger(target, key){
-  const depsMap = targetMap.get(target)
-  const deps = depsMap.get(key)
-
-  for(const effect of deps){
-    if(effect.scheduler){
-      effect.scheduler()
-    }else{
-      effect.run()
+	// public scheduler? 这种写法需要注意下，之前不知道
+	constructor(fn, public scheduler?) {
+		this._fn = fn
+		this.scheduler = scheduler
+	}
+	run() {
+		activeEffect = this
+		return this._fn() // 调用结果要返回下
+	}
+	stop() {
+    // 性能优化：避免多次stop
+    if(this.active){
+      cleanupEffect(this)
+      if(this.onStop){
+        this.onStop()
+      }
+      this.active = false
     }
-  }
+	}
 }
 
-let activeEffect;
-export function effect(fn, options: any = {}){
-  const effect = new ReactiveEffect(fn, options.scheduler)
-  effect.run()
-  return effect.run.bind(effect) // 返回一个函数，需要指定下函数内部的this
+function cleanupEffect(effect) {
+	effect.deps.forEach((dep: any) => {
+		dep.delete(effect)
+	})
+}
+
+const targetMap = new Map()
+export function track(target, key) {
+	let depsMap = targetMap.get(target)
+	if (!depsMap) {
+		depsMap = new Map()
+		targetMap.set(target, depsMap)
+	}
+	let dep = depsMap.get(key)
+	if (!dep) {
+		dep = new Set()
+		depsMap.set(key, dep)
+	}
+
+  if(!activeEffect) return // 单纯获取响应式对象的属性时是undefined（指的是不在effect中时获取）
+	dep.add(activeEffect)
+	activeEffect.deps.push(dep)
+}
+
+export function trigger(target, key) {
+	const depsMap = targetMap.get(target)
+	const deps = depsMap.get(key)
+
+	for (const effect of deps) {
+		if (effect.scheduler) {
+			effect.scheduler()
+		} else {
+			effect.run()
+		}
+	}
+}
+
+export function stop(runner) {
+	runner.effect.stop()
+}
+
+let activeEffect
+export function effect(fn, options: any = {}) {
+  // fn
+	const _effect = new ReactiveEffect(fn, options.scheduler)
+  // options
+  // effect.onStop = options.onStop
+  // Object.assign(_effect, options) // 更优雅的挂
+  extend(_effect, options)
+	_effect.run()
+
+	const runner: any = _effect.run.bind(_effect)
+	runner.effect = _effect
+	return runner // 返回一个函数，需要指定下函数内部的this
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,64 +1,90 @@
 import { reactive } from './../reactive'
-import {effect} from './../effect'
+import { effect, stop } from './../effect'
 describe('effect', () => {
-  it('happy path', () => {
-    const user = reactive({
-      age: 10
-    })
-    let nextAge;
-    effect(() => {
-      nextAge = user.age + 1
-    })
-    // init
-    expect(nextAge).toBe(11)
-    
-    // update
-    user.age ++
-    expect(nextAge).toBe(12)
-  })
+	it('happy path', () => {
+		const user = reactive({
+			age: 10,
+		})
+		let nextAge
+		effect(() => {
+			nextAge = user.age + 1
+		})
+		// init
+		expect(nextAge).toBe(11)
 
-  it('should return runner when call effect', () => {
-    let foo = 10
-    const runner = effect(() => {
-      foo ++
-      return 'foo'
-    })
-    expect(foo).toBe(11)
-    const r = runner()
-    expect(foo).toBe(12)
-    expect(r).toBe('foo')
-  })
+		// update
+		user.age++
+		expect(nextAge).toBe(12)
+	})
 
-  it('scheduler', () => {
-    // 1. effect 第二个参数给定一个 scheduler 的fn
-    // 2. effect 第一次执行的时候，还会执行 fn
-    // 3. 当响应式对象 set update 时不会执行 fn 而是执行 scheduler
-    // 4. 当执行 runner 时，会再次执行 fn
-    let dummy
-    let run: any
-    const scheduler = jest.fn(() => {
-      run = runner
-    })
-    const obj = reactive({ foo: 1 })
-    const runner = effect(
-      () => {
-        dummy = obj.foo
-      },
-      { scheduler }
-    )
-    expect(scheduler).not.toHaveBeenCalled()
-    expect(dummy).toBe(1)
-    // should be called on first trigger
-    obj.foo++
-    expect(scheduler).toHaveBeenCalledTimes(1)
-    // should not run yet
-    expect(dummy).toBe(1)
-    // manually run
-    run()
-    // should have run
-    expect(dummy).toBe(2)
+	it('should return runner when call effect', () => {
+		let foo = 10
+		const runner = effect(() => {
+			foo++
+			return 'foo'
+		})
+		expect(foo).toBe(11)
+		const r = runner()
+		expect(foo).toBe(12)
+		expect(r).toBe('foo')
+	})
 
-    // 加这个功能时没看vue3的实现时，有个疑惑的点：set update的时候scheduler调用，难道要将scheduler也放在map里一一对应收集下吗？看了下实现，脑子果然秀逗了，已经收集过一遍effect,scheduler又是和effect对应的，直接在effect加个scheduler即可
-  })
+	it('scheduler', () => {
+		// 1. effect 第二个参数给定一个 scheduler 的fn
+		// 2. effect 第一次执行的时候，还会执行 fn
+		// 3. 当响应式对象 set update 时不会执行 fn 而是执行 scheduler
+		// 4. 当执行 runner 时，会再次执行 fn
+		let dummy
+		let run: any
+		const scheduler = jest.fn(() => {
+			run = runner
+		})
+		const obj = reactive({ foo: 1 })
+		const runner = effect(
+			() => {
+				dummy = obj.foo
+			},
+			{ scheduler }
+		)
+		expect(scheduler).not.toHaveBeenCalled()
+		expect(dummy).toBe(1)
+		// should be called on first trigger
+		obj.foo++
+		expect(scheduler).toHaveBeenCalledTimes(1)
+		// should not run yet
+		expect(dummy).toBe(1)
+		// manually run
+		run()
+		// should have run
+		expect(dummy).toBe(2)
 
+		// 加这个功能时没看vue3的实现时，有个疑惑的点：set update的时候scheduler调用，难道要将scheduler也放在map里一一对应收集下吗？看了下实现，脑子果然秀逗了，已经收集过一遍effect,scheduler又是和effect对应的，直接在effect加个scheduler即可
+	})
+
+	it('stop', () => {
+		let dummy
+		const obj = reactive({ prop: 1 })
+		const runner = effect(() => {
+			dummy = obj.prop
+		})
+		obj.prop = 2
+		expect(dummy).toBe(2)
+		stop(runner)
+		obj.prop = 3
+		expect(dummy).toBe(2)
+
+		// stopped effect should still be manually callable
+		runner()
+		expect(dummy).toBe(3)
+	})
+
+	it('events: onStop', () => {
+		const onStop = jest.fn()
+		const runner = effect(() => {}, {
+			onStop,
+		})
+
+		stop(runner)
+		expect(onStop).toHaveBeenCalled()
+	})
 })
