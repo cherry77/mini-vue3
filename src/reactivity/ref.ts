78de808e689f785d02479d53ad5755d2e664c8da
flora <15071285862@163.com>
2022-09-04 18:57:08 +0800

    add isRef && unRef

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -5,12 +5,13 @@ import { reactive } from './reactive'
 class RefImpl {
 	private _value: any
 	public dep
-  private _rawValue: any
+	private _rawValue: any
+	public __v_isRef = true
 
 	constructor(value) {
 		this._value = convert(value) // 如果是对象，需要用reactive
-    this._rawValue = value // 存储原始的值
-    this.dep = new Set()
+		this._rawValue = value // 存储原始的值
+		this.dep = new Set()
 	}
 
 	get value() {
@@ -19,17 +20,17 @@ class RefImpl {
 	}
 
 	set value(newValue) {
-    // set的时候需要跟rawValue判断下，因为this._value已经变成代理对象了
+		// set的时候需要跟rawValue判断下，因为this._value已经变成代理对象了
 		if (!hasChanged(this._rawValue, newValue)) return
 
 		this._value = convert(newValue)
-    this._rawValue = newValue
+		this._rawValue = newValue
 		triggerEffects(this.dep)
 	}
 }
 
-function convert(value){
-  return isObject(value) ? reactive(value) : value
+function convert(value) {
+	return isObject(value) ? reactive(value) : value
 }
 
 function trackRefValue(ref) {
@@ -40,4 +41,12 @@ function trackRefValue(ref) {
 
 export function ref(value) {
 	return new RefImpl(value)
+}
+
+export function isRef(ref) {
+	return !!ref.__v_isRef
+}
+
+export function unRef(ref) {
+	return isRef(ref) ? ref.value : ref
 }
