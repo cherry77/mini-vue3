6a531abd26aabebe91fb7a8673309e070f2cf6b3
flora <15071285862@163.com>
2022-09-06 20:59:32 +0800

    add proxyRefs

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -50,3 +50,18 @@ export function isRef(ref) {
 export function unRef(ref) {
 	return isRef(ref) ? ref.value : ref
 }
+
+export function proxyRefs(objectWithRefs) {
+	return new Proxy(objectWithRefs, {
+		get(target, key) {
+			return unRef(Reflect.get(target, key))
+		},
+		set(target, key, newValue) {
+			if (isRef(target[key]) && !isRef(newValue)) {
+				return (target[key].value = newValue)
+			} else {
+				return Reflect.set(target, key, newValue)
+			}
+		},
+	})
+}
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from "../effect"
 import { reactive } from "../reactive"
-import { isRef, ref, unRef } from "../ref"
+import { isRef, proxyRefs, ref, unRef } from "../ref"
 
 describe('ref', () => {
 	it('should hold a value', () => {
@@ -55,5 +55,28 @@ describe('ref', () => {
     const a = ref(1)
     expect(unRef(a)).toBe(1)
     expect(unRef(1)).toBe(1)
+  })
+
+  it('proxyRefs', () => {
+    const user = {
+      age: ref(10),
+      name: "cherry"
+    }
+
+    const proxyUser = proxyRefs(user)
+
+    // get
+    expect(user.age.value).toBe(10)
+    expect(proxyUser.age).toBe(10)
+    expect(proxyUser.name).toBe('cherry')
+
+    // set
+    proxyUser.age = 20
+    expect(proxyUser.age).toBe(20)
+    expect(user.age.value).toBe(20)
+
+    proxyUser.age = ref(10)
+    expect(proxyUser.age).toBe(10)
+    expect(user.age.value).toBe(10)
   })
 })
